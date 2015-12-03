5be07a5a55c9e3918abafe45ddce20fab660e356
Antonio Menarde <amenarde@gmail.com>
2015-12-02 20:42:38 -0500

    mega serial testing update

--- Mega_Serial_Test/Mega_Serial_Test/Mega_Serial_Test.ino
+++ Mega_Serial_Test/Mega_Serial_Test/Mega_Serial_Test.ino
@@ -1,7 +1,3 @@
-
-#include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(11, 10); // RX, TX
 int overall = 1; 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -9,24 +5,15 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
-
-  Serial.println("Goodnight moon!");
-  Serial.println("WE DID IT UNO!");
-
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
-  mySerial.println("Hello, Uno world?");
+  Serial1.begin(9600);
 }
 
 void loop() { // run over and over
 
   
-    mySerial.write("1"); 
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
-  }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
+//  delay(1000);
+  while(Serial1.available() > 0) {
+    Serial.print(Serial1.readString());
+    Serial1.print("2");
   }
 }
