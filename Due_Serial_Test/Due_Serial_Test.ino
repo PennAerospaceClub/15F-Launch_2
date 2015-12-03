cb23e681226a1993932b01476192dd576b451e0e
adam-zachar <zachara@seas1297.wireless-pennnet.upenn.edu>
2015-12-02 20:41:36 -0500

    Updated Due Serial Test

--- Due_Serial_Test/Due_Serial_Test.ino
+++ Due_Serial_Test/Due_Serial_Test.ino
@@ -1,7 +1,7 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial arduinoSerial(10, 11); // RX, TX
+SoftwareSerial arduinoSerial(12, 13); // RX, TX
 int overall = 0; 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -11,8 +11,8 @@ void setup() {
   }
 
 
-  Serial.println("Goodnight moon!");
-  Serial.println("WE DID IT MEGA!"); 
+//  Serial.println("Goodnight moon!");
+//  Serial.println("WE DID IT MEGA!"); 
 
   // set the data rate for the SoftwareSerial port
   arduinoSerial.begin(9600);
@@ -22,13 +22,13 @@ void setup() {
 
 void loop() { // run over and over
 
-    arduinoSerial.print("0");
-    delay(1000);
-   if (arduinoSerial.available()) {
-    Serial.write(arduinoSerial.read());
-  }
-   if (Serial.available()) {
-    arduinoSerial.write(Serial.read());
-    
-  }
+ delay(1000);
+ arduinoSerial.print("3");
+ while (arduinoSerial.available()>0) {
+  Serial.print(arduinoSerial.readString());
+ }
+
+//   if (Serial.available()) {
+//    arduinoSerial.write(Serial.read());
+//      }
 }
